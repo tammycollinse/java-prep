e9e6f35155615573e5c16fea4dba0f99d593937d
Tammy <tammycollinse@gmail.com>
2022-12-13 11:47:02 -0500

    some notes

---
+++ index.js
@@ -0,0 +1,48 @@
+//These are some java notes
+//This is from coursera
+
+//This is core Java Specialization
+//Introduction => features and benefits, fundamentals
+//Introduction to OOP
+//Deep dive into OOP
+
+//MODULE INTRODUCTION AND BENEFITS OF JAVA
+
+//Write One Run Anywhere (WORA)
+//OO
+//Familiar C/C++ based syntax
+//rich libaries
+//security
+//performance
+//its free
+//community process for expanding java
+
+//Java App is hosted on Java Virtual Machine
+//The code is compiled into an intermediate format called architecture neutral bytecodes
+//developers write code for the java platformrather than a specific platform
+
+//byte codes are executed by the JVM and the bytecode instructions are translated into native machine code by the JRE
+//Java runtime environment
+
+//THE PROCESS OF COMPILATION TO EXECUTION
+//Java source file => compiler => java bytecode => bytecode verifier class loader JIT compiler (this is the JRE)=> native code, OS, Hardware platform
+
+//Taking a look at key benefits: PACKAGES, syntax and libaries
+//a package is some neutral classes => think of a package like a directory
+//just as windows directories are seperates by the backslash, the nodes of Jaca packages are structured and seperated by dots
+//e.g package com.learnquest.calculator.util;
+//packages help us group files and classes more meaningfully
+
+//for each primitive, there is a wrapper class, there are object literals with data types
+
+//Java has LIBRARY STANDARD
+//these lirbabries are based on the standards developed within and by the Java community
+//JCP => java community process defines:
+//a specification of the API , interfaces and required behavior
+//designed to enhance portablity by promoting vendor independence
+
+
+//  DATABASE CONNECTIVITY, Java Database Connectivity 
+//JDBC packages are a key offering found in java.sql
+//thisn package facilitates database neutral interaction with a relational database
+//theb packages provides a set of interfaces that have been standardized by the JDBC specification
